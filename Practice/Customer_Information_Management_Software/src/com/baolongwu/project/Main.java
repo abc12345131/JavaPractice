34523ab996c307505fb2c2a986fd536ff85f9791
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2023-05-19 09:10:06 -0400

    add small practices

---
+++ Practice/Customer_Information_Management_Software/src/com/baolongwu/project/Main.java
@@ -0,0 +1,9 @@
+package com.baolongwu.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CustomerView cView = new CustomerView();
+        cView.enterMainMenu();
+    }
+}
