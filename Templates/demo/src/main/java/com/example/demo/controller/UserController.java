6c5ed295ac9037e936588247405c83d82f0a268e
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2023-05-26 03:38:41 -0400

    add springboot

---
+++ Templates/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author abc12345131
+ * @create 2023/5/19-17:33
+ */
+
+@RestController
+@RequestMapping(value="/user")
+public class UserController {
+
+    @GetMapping(value="/name")
+    public String getUserName(){
+
+        return "ABC";
+    }
+
+}
